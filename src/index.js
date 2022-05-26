4e6c5dd0e93d89cb7c13f6492e174727ec01fbe0
jegoc <73209782+jegoc@users.noreply.github.com>
2022-05-26 14:28:22 +0800

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+// import {HashRouter} from 'react-router-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import rootReducers from './redux/reducers/reducers';
+import { createStore, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { Provider } from 'react-redux';
+// import thunk from 'redux-thunk';
+import saga from './redux/saga'
+
+const composeEnhancers = composeWithDevTools({ trace: true, traceLimit: 25 });
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(rootReducers, composeEnhancers(applyMiddleware(sagaMiddleware)));
+
+sagaMiddleware.run(saga);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+      <App />
+  </Provider>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
